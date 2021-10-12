5dbbb3d5af66ee3077a6deb2ac29755dd82f3923
Tevissaur <berserkboar@protonmail.com>
2021-10-12 16:18:48 -0500

    working db out

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO department (name)
+VALUES ("Produce"),
+       ("Meat"), 
+       ("Specialty"), 
+       ("Grocery"), 
+       ("Bakery"), 
+       ("Deli"), 
+       ("Coffee Bar")
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Barista", 50.00, 7),
+       ("Baker", 50.00, 5), 
+       ("Cheese Monger", 50.00, 3), 
+       ("Butcher", 50.00, 2), 
+       ("Deli Attendent", 50.00, 6), 
+       
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES ("Produce"),
+--        ("Meat"), 
+       
+         
